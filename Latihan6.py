e75a7038ede4b6dc7132f664e6b1bbfcc0abee3f
muhamaddanang655-netizen <muhamaddanang655@gmail.com>
2026-01-22 13:08:54 +0700

    UAS

---
+++ Latihan6.py
@@ -0,0 +1,8 @@
+# Definisikan fungsi untuk menyatakan perasaan
+def kondisi (perasaan):
+    if kondisi == "senang":
+        return "Saya jika ini UAS Dapat A!"
+    elif kondisi == "sedih":
+        return "Saya saya sedih karena mengerjakan menggunakan ChatGPT."
+    else:
+        return "saya akan mengulang perkuliahan ini dengan baik."
---
+++ Latihan7.py
@@ -0,0 +1,11 @@
+# import fungsi dari Latihan6.py
+def kondisi (perasaan):
+    if kondisi == "senang":
+        return "Saya jika ini UAS Dapat A!"
+    elif kondisi == "sedih":
+        return "Saya saya sedih karena mengerjakan menggunakan ChatGPT."
+    else:
+        return "saya akan mengulang perkuliahan ini dengan baik."
+
+# print hasil pemanggilan fungsi menyatakan_perasaan dengan argumen "senang"
+print(("senang"))
